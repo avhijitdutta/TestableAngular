4a516f25db7b0efa0d78197f0450fcd3d61e0926
Avhijit Dutta <avhijitduta@yahoo.in>
2016-11-28 01:57:29 +0530

    project setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contactMgmtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the contactMgmtApp
+ */
+angular.module('contactMgmtApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
